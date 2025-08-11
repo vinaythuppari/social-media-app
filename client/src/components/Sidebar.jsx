b4f6b85da5a42e118c658c34cb234aa90f137b73
vinaythuppari <warangalabbai4@gmail.com>
2025-08-11 20:32:46 +0530

    side bar setup part-1

---
+++ client/src/components/Sidebar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
+import MenuItems from './MenuItems'
+
+const Sidebar = ({sidebarOpen, setSidebarOpen}) => {
+
+    const navigate = useNavigate()
+
+  return (
+    <div className={`w-60 xl:w-72 bg-white border-r border-gray-200 flex flex-col
+    justify-between items-center max-sm:absolute top-0 bottom-0 z-20 
+    ${sidebarOpen ? 'translate-x-0' : 'max-sm:-translate-x-full'} transition-all
+    duration-300 ease-in-out `}>
+        <div className='w-full'>
+            <img onClick= {()=>navigate('/')} src={assets.logo} alt="logo" 
+             className='w-26 ml-7 my-2 cursor-pointer' />
+             <hr className='border-gray-300 mb-8'/>
+
+             <MenuItems setSidebarOpen={setSidebarOpen}/>
+
+        </div>
+    </div>
+  )
+}
+
+export default Sidebar
